32e4d080359a45c95c2ea43ea859908dfe716a40
Andy Bravo <anbrav@hotmail.com>
2016-12-14 13:13:59 -0600

    Se modifica el servicio para recibir como parametro la funcion a evaluar

--- app/calculator.service.ts
+++ app/calculator.service.ts
@@ -1,5 +1,5 @@
 import { Injectable }     from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Http, Response, Headers, RequestOptions,URLSearchParams } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
@@ -7,17 +7,22 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class CalculatorService {
-  
-  constructor(private http: Http){}
 
-  evaluate(exp : string) :Observable<string>{
-  	return this.http.get("http://localhost:8080/evaluate?expression=4%2b3")
-  		.map((responseData) => {
-  			console.log("responseData:" + responseData);				
-  			return responseData.json().content;
-  		})
+	constructor(private http: Http){}
+
+	evaluate(exp : string) :Observable<string>{
+		let params: URLSearchParams = new URLSearchParams();
+		params.set('expression', exp);
+
+		return this.http.get("http://localhost:8080/evaluate", {
+		   search: params
+		 })
+		.map((responseData) => {
+			console.log("responseData:" + responseData);				
+			return responseData.json().content;
+		})
 		.catch((error: any) => Observable.throw(error.json().error || 'Server error'));
 
-  }
+	}
 
 }
